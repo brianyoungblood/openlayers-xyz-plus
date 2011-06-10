3964a5dd35e3d1238d6b9eed36e94f8c048c11b4
brianyoungblood <brian.youngblood@splcenter.org>
2011-06-10 14:17:41 -0500

    adding isbaselayer option

---
+++ openlayersxyzplus/includes/layer_types/openlayersxyzplus.js
@@ -0,0 +1,20 @@
+
+/**
+ * @file
+ * Layer handler for OpenLayers XYZ Plus layers
+ */
+
+/**
+ * Openlayer layer handler for XYZ Plus layer
+ */
+Drupal.openlayers.layer.openlayersxyzplus = function(title, map, options) {
+  var styleMap = Drupal.openlayers.getStyleMap(map, options.drupalID);
+  if (options.maxExtent !== undefined) {
+    options.maxExtent = new OpenLayers.Bounds.fromArray(options.maxExtent) || new OpenLayers.Bounds(-20037508.34, -20037508.34, 20037508.34, 20037508.34);
+  }
+  options.projection = 'EPSG:' + options.projection;
+  options.sphericalMercator = true;
+  var layer = new OpenLayers.Layer.XYZ(title, options.base_url, options);
+  layer.styleMap = styleMap;
+  return layer;
+};
